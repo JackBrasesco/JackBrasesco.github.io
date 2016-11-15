3c73e710a62f31158b36535307a645311143283e
JackBrasesco <jacbras@nuevaschool.org>
2016-11-15 08:47:31 -0800

    Classadoo autocommit

--- -KWcpgeIV20aGRLZ9Qpk/script.js
+++ -KWcpgeIV20aGRLZ9Qpk/script.js
@@ -33,13 +33,16 @@ var aniCount = 0
 
 setInterval(function() {
 aniCount = aniCount + 1
-}, 100)
 console.log(aniCount);
+  if (aniCount > 4) {
+    $("#lol").fontcolor(red);
+    console.log("hi")
+  }
+  if (aniCount == 10) {
+    $("#lol").fontcolor(blue);
+    aniCount = 0;
+  }
+}, 500)
 
-if (aniCount > 4) {
-  $("#lol").fontcolor(red);
-}
-if (aniCount == 10) {
-  $("#lol").fontcolor(blue);
-  aniCount = 0;
-}
+
+
