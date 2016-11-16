ce43ead74bcb03550b8a84b05db8df084cfc9fc3
JackBrasesco <jacbras@nuevaschool.org>
2016-11-16 09:34:46 -0800

    Classadoo autocommit

--- -KWd1OS_HLoG-CNQEE-6/taco.js
+++ -KWd1OS_HLoG-CNQEE-6/taco.js
@@ -15,7 +15,7 @@ function tacoClickedAnimation1() {
   $("#tacoTitle").html("You have " + tacoCount + " tacos!" +"<br>" + "You are generating " + tacoPerSecond + " tacos per second");
   setInterval(function() {
     tacoPerSecond = (tacoCount - tacoPerSecond)
-    conosle.log(tacoPerSecond)
+    console.log(tacoPerSecond)
   },1000)
 }
 
