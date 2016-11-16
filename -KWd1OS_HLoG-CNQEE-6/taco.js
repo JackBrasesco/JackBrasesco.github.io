02fc799a7784a06df9016ac3caff75be531921e8
JackBrasesco <jacbras@nuevaschool.org>
2016-11-16 09:13:37 -0800

    Classadoo autocommit

--- -KWd1OS_HLoG-CNQEE-6/taco.js
+++ -KWd1OS_HLoG-CNQEE-6/taco.js
@@ -1,14 +1,15 @@
 var taco = $("#taco");
 
 taco.click(function() {
-  tacoClickedAnimation1()
-  console.log("hi")
+  tacoClickedAnimation1();
+  console.log("hi");
+  tacoClickedAnimation2();
 })
 
 function tacoClickedAnimation1() {
   taco.css({background: "grey"});
 }
-
+setTimeout
 function tacoClickedAnimation2() {
   taco.css({background: "white"});
 }
