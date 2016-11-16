55e79b0719a1dd34f9cc32da1397eb4a52ac3c93
JackBrasesco <jacbras@nuevaschool.org>
2016-11-16 08:52:24 -0800

    Classadoo autocommit

--- -KWd1OS_HLoG-CNQEE-6/taco.js
+++ -KWd1OS_HLoG-CNQEE-6/taco.js
@@ -1,7 +1,8 @@
-var signinbtn = $("#a");
-var signupbtn = $("#b");
+var signunbtn = $("#a");
+var signipbtn = $("#b");
 var bobbtn = $("#c");
 var chatbtn = $("#d");
 var heybtn = $("#e");
 
 
+signupbtn.click(funtion()
